a38eafb30192fea81878a335a55dfe1d0918c3c3
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-09 09:29:26 +0700

    starter-project

---
+++ functions/db.php
@@ -0,0 +1,10 @@
+<?php 
+
+$host       = "localhost";
+$user       = "root";
+$user       = "root";
+$db         = "self_health";
+
+$link = new mysqli($host, $user, $password, $db) or die ("error connection");
+
+?>
